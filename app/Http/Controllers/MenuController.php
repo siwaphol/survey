ceb5ede4999ad1c4b54af5199874a2f75ab5d0f3
Nong <nong-siwa@hotmail.com>
2016-07-19 18:36:51 +0700

    latest update

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Menu;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -10,7 +11,20 @@ class MenuController extends Controller
 {
     public function index()
     {
-        
+        $result = [];
+        $menus = Menu::orderBy('parent_id','ASC')
+            ->orderBy('order', 'ASC')
+            ->get();
+
+        foreach ($menus as $row){
+            
+        }
+
         return json_encode(['data'=>[]]);
+    }
+
+    protected function createTree($data, &$result)
+    {
+
     }
 }
