5a8561917163c205be691a72016980dae72735fb
Nong <nong-siwa@hotmail.com>
2016-07-27 21:53:44 +0700

    only new create main should has predefined answer

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -42,17 +42,21 @@ class MainController extends Controller
                 'recorder_id'=>$user_id
             ]);
 
-            // predefined answer ตั้งแต่ ค.1 ให้เลือก ไม่มี ทั้งหมด
             $mainId = $request->input('main_id');
-            Answer::create(['main_id'=>$mainId, 'section'=>'ค.1 หมวดแหล่งเชื้อเพลิงที่หาเองได้', 'sub_section'=>'NULL', 'option_question_id'=>'486', 'question_id'=>'800']);
-            Answer::create(['main_id'=>$mainId, 'section'=>'ค.2 หมวดแหล่งเชื้อเพลิงที่ซื้อ', 'sub_section'=>'NULL', 'option_question_id'=>'499', 'question_id'=>'808']);
-            Answer::create(['main_id'=>$mainId, 'section'=>'ง.1 การเผาถ่าน', 'sub_section'=>'NULL', 'option_question_id'=>'536', 'question_id'=>'700']);
-            Answer::create(['main_id'=>$mainId, 'section'=>'ง.2 การผลิตก๊าซชีวภาพ', 'sub_section'=>'NULL', 'option_question_id'=>'551', 'question_id'=>'716']);
-            Answer::create(['main_id'=>$mainId, 'section'=>'ง.3 เครื่องปั่นไฟ', 'sub_section'=>'NULL', 'option_question_id'=>'561', 'question_id'=>'729']);
-            Answer::create(['main_id'=>$mainId, 'section'=>'ง.4 การผลิตไฟฟ้าด้วยเซลล์แสงอาทิตย์', 'sub_section'=>'NULL', 'option_question_id'=>'572', 'question_id'=>'735']);
-            Answer::create(['main_id'=>$mainId, 'section'=>'จ.1 แนวโน้มการเปลี่ยนการใช้พลังงานในการประกอบอาหาร', 'sub_section'=>'NULL', 'option_question_id'=>'584', 'question_id'=>'900']);
-            Answer::create(['main_id'=>$mainId, 'section'=>'จ.2 แนวโน้มการเปลี่ยนการใช้พลังงานในการเดินทางและคมนาคม', 'sub_section'=>'NULL', 'option_question_id'=>'611', 'question_id'=>'904']);
-            Answer::create(['main_id'=>$mainId, 'section'=>'จ.3 แนวโน้มการเปลี่ยนการใช้ยานพาหนะในการเดินทาง', 'sub_section'=>'NULL', 'option_question_id'=>'640', 'question_id'=>'908']);
+            $newMain = Main::where('main_id', $mainId)
+                ->count();
+            if((int)$newMain===1){ // newly created main
+                // predefined answer ตั้งแต่ ค.1 ให้เลือก ไม่มี ทั้งหมด
+                Answer::create(['main_id'=>$mainId, 'section'=>'ค.1 หมวดแหล่งเชื้อเพลิงที่หาเองได้', 'sub_section'=>'NULL', 'option_question_id'=>'486', 'question_id'=>'800']);
+                Answer::create(['main_id'=>$mainId, 'section'=>'ค.2 หมวดแหล่งเชื้อเพลิงที่ซื้อ', 'sub_section'=>'NULL', 'option_question_id'=>'499', 'question_id'=>'808']);
+                Answer::create(['main_id'=>$mainId, 'section'=>'ง.1 การเผาถ่าน', 'sub_section'=>'NULL', 'option_question_id'=>'536', 'question_id'=>'700']);
+                Answer::create(['main_id'=>$mainId, 'section'=>'ง.2 การผลิตก๊าซชีวภาพ', 'sub_section'=>'NULL', 'option_question_id'=>'551', 'question_id'=>'716']);
+                Answer::create(['main_id'=>$mainId, 'section'=>'ง.3 เครื่องปั่นไฟ', 'sub_section'=>'NULL', 'option_question_id'=>'561', 'question_id'=>'729']);
+                Answer::create(['main_id'=>$mainId, 'section'=>'ง.4 การผลิตไฟฟ้าด้วยเซลล์แสงอาทิตย์', 'sub_section'=>'NULL', 'option_question_id'=>'572', 'question_id'=>'735']);
+                Answer::create(['main_id'=>$mainId, 'section'=>'จ.1 แนวโน้มการเปลี่ยนการใช้พลังงานในการประกอบอาหาร', 'sub_section'=>'NULL', 'option_question_id'=>'584', 'question_id'=>'900']);
+                Answer::create(['main_id'=>$mainId, 'section'=>'จ.2 แนวโน้มการเปลี่ยนการใช้พลังงานในการเดินทางและคมนาคม', 'sub_section'=>'NULL', 'option_question_id'=>'611', 'question_id'=>'904']);
+                Answer::create(['main_id'=>$mainId, 'section'=>'จ.3 แนวโน้มการเปลี่ยนการใช้ยานพาหนะในการเดินทาง', 'sub_section'=>'NULL', 'option_question_id'=>'640', 'question_id'=>'908']);
+            }
         }
 
         \Session::put('main_id', $main->main_id);
