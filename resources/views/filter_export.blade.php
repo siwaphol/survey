c17ee8f57a9e87a70dee77c8bbf2761de3a220a4
Nong <nong-siwa@hotmail.com>
2016-09-12 15:42:41 +0700

    add filter export controller and view

---
+++ resources/views/filter_export.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.afterlogin')
+
+@section('script')
+
+@endsection
+
+@section('content')
+
+    <div class="content">
+        <div class="panel panel-flat">
+            <div class="panel-heading">
+                <h5 class="panel-title">filter</h5>
+            </div>
+            <div class="panel-body">
+
+            </div>
+        </div>
+    </div>
+
+@endsection
