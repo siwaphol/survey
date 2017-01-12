6a63b1e18b89315dadfd2a0f08a6fb338f390d16
Nong <nong-siwa@hotmail.com>
2017-01-12 18:11:20 +0700

    update UploadRawController

--- app/Http/Controllers/UploadRawController.php
+++ app/Http/Controllers/UploadRawController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Answer;
 use App\Custom\ChunkReadFilter;
 use App\Menu;
+use App\OptionQuestion;
 use App\Question;
 use Illuminate\Http\Request;
 
@@ -34,7 +35,7 @@ class UploadRawController extends Controller
         }elseif (substr($explodedUK[count($explodedUK)-1],0,1)==="o"){
             return Question::TYPE_CHECKBOX;
         }elseif (substr($explodedUK[count($explodedUK)-1],0,2)==="te"){
-            return Question::TYPE_TEXT;s
+            return Question::TYPE_TEXT;
         }
 
         return null;
@@ -53,60 +54,95 @@ class UploadRawController extends Controller
         $chunkFilter = new ChunkReadFilter();
         $objReader->setReadFilter($chunkFilter);
 
+        $errors = array();
 
-
-        for ($startRow = 2; $startRow <= $totalRows; $startRow += $chunkSize) {
+        for ($startRow = 3; $startRow <= $totalRows; $startRow += $chunkSize) {
             $chunkFilter->setRows($startRow,$chunkSize);
 
             $objPHPExcel = $objReader->load($path);
 
             $sheetData = $objPHPExcel->getActiveSheet()->toArray(null,true,true,true);
 
-            for($i=$startRow; $i <= ($startRow+$chunkSize-1); $i++) {
-                if ($i > $totalRows) {
-                    break;
+            // loop ไปทีละคอลัมป์ เพราะดูเหมือนว่าตอนนี้มีการ query หลายครั้ง ที่ไม่จำเป็น ถ้าวน row ก่อนวน column
+            // แถวแรกต้องเป็น unique_key
+
+            $curColumn = "B";
+            // column loop each unique_key
+            while (!empty(trim($sheetData[1][$curColumn]))){
+                $uniqueKey = trim($sheetData[1][$curColumn]);
+                // อาจจะเกิดปัญหาว่าบางที uniqueKey ที่ใส่เข้ามาผิดแล้วจะทำให้ค่าที่ insert เข้าไปผิดไปด้วย
+                // อาจจะแก้ปัญหาโดยการหาว่ามี unique_key นี้ในระบบหรือไม่
+                // หาในตาราง answers ว่ามี unique_key นี้มั้ย ถ้าไม่มีก็ ข้ามคอลัมน์ปัจจุบันไปทำคอลัมน์ถัดไป
+                $uniqueKeyAnswer = Answer::where('unique_key', $uniqueKey)->first();
+                if (is_null($uniqueKeyAnswer)){
+                    $errors[] = 'Not found unique key ' . $uniqueKey .' in system';
+                    continue;
                 }
-
-                //TODO-nong put loop for each unique_key here
-
-                //TODO-nong this variable will be used to store unique_key
-                $uniqueKey = "";
-                //TOTO-nong this variable will store current column
-                $curColumn = "B";
-                if(is_numeric(trim($sheetData[$i]["A"]))){
-                    $mainId = trim($sheetData[$i]["A"]);
-                    $oldAnswer = Answer::where('main_id', $mainId)
-                        ->where('unique_key', $uniqueKey)
-                        ->first();
-
-                    //TODO-nong อาจจะเกิดปัญหาว่าบางที uniqueKey ที่ใส่เข้ามาผิดแล้วจะทำให้ค่าที่ insert เข้าไปผิดไปด้วย
-                    // 1. ตรวจว่าเคยมี uniqueKey นี้ในตาราง answer มั้ย?
-                    if (is_null($oldAnswer)){
-                        $oldAnswer = new Answer();
-                        $oldAnswer->main_id = $mainId;
-                        $oldAnswer->unique_key = $uniqueKey;
+                // row loop
+                for($i=$startRow; $i <= ($startRow+$chunkSize-1); $i++) {
+                    if ($i > $totalRows) {
+                        break;
                     }
 
-                    $newValue = $sheetData[$i][$curColumn];
+                    if(is_numeric(trim($sheetData[$i]["A"]))){
 
-                    // เปลี่ยนค่าตรงนี้
-                    // ถ้า unique_key บอกว่าเป็น text หรือ number input ให้ใส่ค่าเข้าไปทันที
-                    if ($this->checkAnswerType($uniqueKey)===Question::TYPE_TEXT){
-                        $oldAnswer->answer_text = $newValue;
-                    }elseif ($this->checkAnswerType($uniqueKey)===Question::TYPE_NUMBER){
-                        $oldAnswer->answer_numeric = (float)$newValue;
-                    }elseif ($this->checkAnswerType($uniqueKey)===Question::TYPE_RADIO){
-                        //TODO-nong get list of all option that come with this radio
-                        // and find if text in newValue match any option name
-                        // then insert that option_id
-                    }elseif ($this->checkAnswerType($uniqueKey)===Question::TYPE_CHECKBOX){
+                        $newValue = $sheetData[$i][$curColumn];
+                        // ถ้าค่าที่ใส่มาเป็นช่องเปล่าข้ามไปแถวต่อไป
+                        if (empty($newValue))
+                            continue;
+
+                        $mainId = trim($sheetData[$i]["A"]);
+                        $oldAnswer = Answer::where('main_id', $mainId)
+                            ->where('unique_key', $uniqueKey)
+                            ->first();
+
+                        if (is_null($oldAnswer)){
+                            $oldAnswer = new Answer();
+                            $oldAnswer->main_id = $mainId;
+                            $oldAnswer->unique_key = $uniqueKey;
+                            $oldAnswer->section_id = $uniqueKeyAnswer->section_id;
+                            $oldAnswer->sub_section_id = $uniqueKeyAnswer->sub_section_id;
+                            $oldAnswer->option_question_id = $uniqueKeyAnswer->option_question_id;
+                            $oldAnswer->question_id = $uniqueKeyAnswer->question_id;
+                        }
+
+                        // เปลี่ยนค่าตรงนี้
+                        // ถ้า unique_key บอกว่าเป็น text หรือ number input ให้ใส่ค่าเข้าไปทันที
+                        if ($this->checkAnswerType($uniqueKey)===Question::TYPE_TEXT){
+                            $oldAnswer->answer_text = $newValue;
+                        }elseif ($this->checkAnswerType($uniqueKey)===Question::TYPE_NUMBER){
+                            $oldAnswer->answer_numeric = (float)$newValue;
+                        }elseif ($this->checkAnswerType($uniqueKey)===Question::TYPE_RADIO){
+                            // ** this one is tricky เพราะ 1 radio สามารถมีได้หลาย option_id
+                            //TODO-nong get list of all option that come with this radio
+                            // and find if text in newValue match any option name
+                            // then insert that option_id
+                            $explodedUK = explode("_",$uniqueKey);
+                            $radioId = filter_var($explodedUK[count($explodedUK)-1],FILTER_SANITIZE_NUMBER_INT );
+                            $curRadioOptions = OptionQuestion::where('question_id', $radioId)
+                                ->leftJoin('options')
+                                ->on('option_questions.option_id','=','options.id')
+                                ->select(\DB::raw('options.id as option_id, options.name as option_name'))
+                                ->get();
+                            // หาว่าค่าที่รับเข้ามาอยู่ใน option ไหน
+                            foreach ($curRadioOptions as $item){
+                                if (strpos($item->option_name,$newValue)!==false){
+                                    $oldAnswer->option_id = $item->option_id;
+                                    break;
+                                }
+                            }
+
+                        }elseif ($this->checkAnswerType($uniqueKey)===Question::TYPE_CHECKBOX){
+                            $oldAnswer->option_id = $uniqueKeyAnswer->option_id;
+                        }
+
+                        $oldAnswer->save();
 
                     }
-
-                    $oldAnswer->save();
-
                 }
+
             }
+
         }
     }
 }
