75bab9a18c1640fc5d34fdc9ece4e6016dc158a4
Nong <nong-siwa@hotmail.com>
2017-01-14 12:52:24 +0700

    เพิ่ม system_configs table เพื่อหาดูว่าตาราง answers update ล่าสุดเมื่อไหร่ จะได้ไม่ต้องคำนวณ dashboard ใหม่ทุกครั้ง

---
+++ app/SystemConfig.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SystemConfig extends Model
+{
+    const ANSWERS_TABLE_LAST_UPDATE = 'answers_table_last_update';
+
+    protected $casts = [
+        'value'=>'array'
+    ];
+}
